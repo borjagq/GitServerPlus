a57083cd056b22fc6a760486dc66c5e35c09b9d2
Borja García Quiroga <borja.garcia@1d3a.com>
2021-03-25 23:43:00 +0100

    Package Info
    Completes the package info as well as the package directory structure.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Create a package.tgz file containing the package folder and files.
+cd ./GitServerPlus/package
+tar -cvzf ../package.tgz --exclude='.DS_Store' --exclude='.git' *
+
+# Create the SPK file.
+# Prevent the package folder and files to be included in the SPK, as they are already in the TGZ.
+cd ..
+tar -cvf ../GitServerPlus.spk --exclude='package' --exclude='.DS_Store' --exclude='.git' *
+
+# Delete the temp TGZ.
+rm ./package.tgz
